99cb94bc818f8b719ec15cf7892562fbb89c4e51
Rafał Major <rafal.major@gmail.com>
2021-05-27 21:57:01 +0200

    Stage VII

--- examples/TimeOfDay.cs
+++ examples/TimeOfDay.cs
@@ -1,10 +1,26 @@
+using System;
+
 namespace examples
 {
     public class TimeOfDay
     {
-        public int Hour { get; }
-        public int Minute { get; }
-        public void NextHour(){}
-        public void NextMinute(){}
+        private readonly int minute;
+        const int minutesInHour = 60;
+        const int hoursInDay = 24;
+        const int minutesInDay = hoursInDay * minutesInHour;
+        public TimeOfDay(int minute)
+        { this.minute = minute % minutesInDay; }
+        public TimeOfDay(int hour, int minute)
+        { 
+            if (hour < 0 || hour >= hoursInDay || minute < 0 || minute >= minutesInHour)
+                throw new ArgumentException();
+            this.minute = minute; 
+        }
+        public int Hour => this.minute / minutesInHour;
+        public int Minute => this.minute;
+        public TimeOfDay WithHour(int newHour){ return new TimeOfDay(newHour); }
+        public TimeOfDay WithMinute(int newMinute){ return new TimeOfDay(this.Hour, newMinute); }
+        public TimeOfDay NextHour(){ return new TimeOfDay(minute+minutesInHour); }
+        public TimeOfDay NextMinute(){ return new TimeOfDay(minute+1); }
     }
 }
