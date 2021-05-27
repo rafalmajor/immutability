aaf430781ac819eb5098ee0b395db05133ca5fb0
Rafał Major <rafal.major@gmail.com>
2021-05-27 21:06:00 +0200

    Step II

--- examples/UnitTest.cs
+++ examples/UnitTest.cs
@@ -1,33 +1,62 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace examples
 {
     public class UnitTest
     {
-[Fact]
-public void Test()
-{
-    var now = Clock.Instance.Now;
-}
-    }
+        private static Clock clockInstance = Clock.Instance;
 
-public class Clock
-{
-    public TimeOfDay Now { get; set; }
-    public static Clock Instance 
-    { 
-        get
+        [Theory]
+        [MemberData(nameof(GetClock))]
+        public void Test(Clock clock)
         {
-            if (instance == null)
-                instance = new Clock();
-            return instance;
+            var now = clock.Now;
+        }
+
+        public static IEnumerable<object[]> GetClock()
+        {
+            yield return new object[] { Clock.Instance };
         }
     }
-    private static Clock instance;
-}
+
+    public class Clock
+    {
+        public TimeOfDay Now { get; set; }
+        public static Clock Instance 
+        { 
+            get
+            {
+                if (instance == null)
+                    instance = new Clock();
+                return instance;
+            }
+        }
+        private static Clock instance;
+    }
 
     public class TimeOfDay
     {
+    }
+
+    public class TestDataGenerator : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                Clock.Instance
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return new object[]
+            {
+                Clock.Instance
+            };
+        }
     }
 }
