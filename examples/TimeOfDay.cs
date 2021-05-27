187b84a17188ad05ea5dec1445c339a7c41f03e4
Rafał Major <rafal.major@gmail.com>
2021-05-27 21:30:37 +0200

    Stage V

---
+++ examples/TimeOfDay.cs
@@ -0,0 +1,10 @@
+namespace examples
+{
+    public class TimeOfDay
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public void NextHour(){}
+        public void NextMinute(){}
+    }
+}
